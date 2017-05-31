298932d5425033fe019ef3fed2226f2f919b49ab
poonam patel <patel.poonam4@gmail.com>
2017-06-01 00:12:37 +0530

    port modified

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const fs = require('fs');
 // const beautify = require("json-beautify");
 const hbs = require('hbs');
-const port = process.env.PORT|3000;
+const port = process.env.PORT||3000;
 console.log("port = ", process.env.PORT);
 
 var app = express();
